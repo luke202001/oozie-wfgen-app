dbccb6b1b8626659b19585bdf29353e38fb608be
Shakeel Ansari <shakeelansari63@gmail.com>
2018-04-29 23:51:53 +1000

    First Version

---
+++ src/main/java/org/oozie/wfgen_app/App.java
@@ -0,0 +1,13 @@
+package org.oozie.wfgen_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
